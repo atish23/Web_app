4fabcc3d5158cefac0c8e7243431d15b94d8fc56
Atish <amaske@gmail.com>
2016-10-23 22:41:11 +0530

    d

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
+  
+
   root 'home#index'
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  
+  resource :subscribes
   
 
 end
